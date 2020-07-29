9a628b31fb57c1b732518613da64313792fbd5f7
zhaiyunfan20000324@qq.com <30565458+zhaiyunfan@users.noreply.github.com>
2020-07-29 23:49:17 +0800

    edit logic code

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import sys
-from PyQt5.QtCore import QTime
-from PyQt5.QtWidgets import QApplication, QMainWindow
 from functools import partial
+
+from PyQt5.QtCore import QTime, QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 import Window
 
+# def cal():
+
 def init(ui):
-    ui.spinBoxHead.maximum = ui.spinBoxMax.value
-    input = ui.spinBoxHead.value
-    timeLeft = 0
-    timeLeft = input * 6
-    hourLeft = 0
-    hourLeft = timeLeft % 60
+    ui.spinBoxHead.maximum = ui.spinBoxMax.value()
+    input = ui.spinBoxHead.value()
+    timeLeft = (ui.spinBoxMax.value() - input) * 6
+    hourLeft = timeLeft // 60
     minuiteLeft = timeLeft - 60 * hourLeft
-    ui.lcdNumberHour.intValue = int(hourLeft)
-    ui.lcdNumbermin.intValue = int(minuiteLeft)
-    time = QTime.currentTime()
+
+    ui.lcdNumberHour.display(hourLeft)
+    ui.lcdNumberMin.display(minuiteLeft)
+
+    # timer.start(60000)
+    # time = QTime.currentTime()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -23,8 +28,11 @@ if __name__ == '__main__':
     ui = Window.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    ui.spinBoxHead.maximum = ui.spinBoxMax.value
+    ui.spinBoxHead.maximum = ui.spinBoxMax.value()
+    
+    #timer = QTimer()
+    #timer.timeout.connect(cal)
+    
     ui.pushButton.clicked.connect(partial(init, ui))
     # ui.actionExit.triggered.connect(partial(clickExit, app))
     sys.exit(app.exec_())
-
